827628f018a28997f14e205779ee67000421699d
Bergman Dmitry <16bdidima@gmail.com>
2024-03-12 21:02:09 +0300

    Delete config directory

--- config/links.py
+++
@@ -1,9 +0,0 @@
-
-
-class Links:
-
-    HOST = 'https://www.saucedemo.com'
-
-    INVENTORY = f'{HOST}/inventory.html'
-    CART = f'{HOST}/cart.html'
-    INVENTORY_FIRST = F'/inventory-item.html?id=1'
